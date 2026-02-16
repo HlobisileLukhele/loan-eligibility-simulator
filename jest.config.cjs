fb7a050df9d489f209f445ec079726c16f8deef7
Hlobisile Lukhele <n.thwala03@gmail.com>
2026-02-16 15:09:19 +0200

    feat: add Babel and Jest configuration files for testing setup

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(js|jsx)$": ["babel-jest", { configFile: "./babel.config.cjs" }],
+  },
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
+};
